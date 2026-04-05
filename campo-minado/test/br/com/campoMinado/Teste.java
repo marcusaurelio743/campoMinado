96993fb3d6c5065e2f1bd2d530c340949c8b5415
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-04-05 15:08:30 -0300

    criando o projeto campo minado

---
+++ campo-minado/test/br/com/campoMinado/Teste.java
@@ -0,0 +1,16 @@
+package br.com.campoMinado;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+	@Test
+	void testarIgualADois() {
+		int a = 1 + 1;
+		assertEquals(2,a);
+		
+	}
+
+}
