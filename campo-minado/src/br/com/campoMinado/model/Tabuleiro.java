1c7f8a0b0e815fb505f69bbd4ac0af74af6e5d06
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-04-06 15:44:38 -0300

    criando regras do tabuleiro

--- campo-minado/src/br/com/campoMinado/model/Tabuleiro.java
+++ campo-minado/src/br/com/campoMinado/model/Tabuleiro.java
@@ -1,5 +1,49 @@
 package br.com.campoMinado.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Tabuleiro {
+	private int linhas;
+	private int colunas;
+	private int minas;
+	private final List<Campo> campos = new ArrayList<Campo>();
+	
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		super();
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sorteaMinas();
+	}
+
+	private void sorteaMinas() {
+		
+	}
+
+	private void associarVizinhos() {
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.adicionarVizinhos(c2);
+			}
+		}
+		
+	}
+
+	private void gerarCampos() {
+		for (int i = 0; i < linhas; i++) {
+			for (int j = 0; j < colunas; j++) {
+				campos.add(new Campo(i, j));
+			}
+		}
+		
+	}
+	
+
+
 
 }
